03143f1ea5bc8fefcb1d2ea6931fbde01350d71f
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-03-19 01:45:14 +0600

    li item add

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 document.getElementById('add-btn').addEventListener("click",function(){
-    console.log("hello");
+    const value = document.getElementById("text-input").value;
+    const container = document.getElementById('parent-container');
+    const li = document.createElement("li");
+    li.innerText = value;
+    container.appendChild(li);
 });
